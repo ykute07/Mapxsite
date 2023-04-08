109b089044267bf544ef6dda1fef7a41b85972c6
ykute07 <yogeshkute777@gmail.com>
2023-04-08 12:13:42 +0530

    src

---
+++ src/pages/FooterPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Footer from '../components/footer/Footer'
+
+export default function NavbarPage() {
+  return (
+    <div>
+        <Footer/>
+    </div>
+  )
+}
---
+++ src/pages/NavbarPage..jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+export default function NavbarPage() {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+
